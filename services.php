5a4e3dc87544e504ee099df51e74efe4de8d96c7
Sung Kyu Hong <sungkyuhong93@gmail.com>
2018-07-12 00:44:30 -0400

    fixed layout

--- services.php
+++ services.php
@@ -3,38 +3,29 @@
 <div class="col-sm-12 column">
 
 <div class="row one">
-<div class="layer">
+<div class="layer layer-one">
 		<div class="headers">
-		<h3 class="title">video post</h3>
-		<h3>production</h3>
+		<h3 class="title">Moderation and Curation</h3>
+		<p class="row-description">Otomo Digital offers a selection of moderators that oversee and review all varieties of media. Our moderators check content for Legal, Copyright and Intellectual Property Compliance. We  accept, label and reject digital assets to ensure legal safety for all of our clients.</p>
 		</div>
 </div>
 </div>
 
-
-<div class="row two">
-<div class="layer">
-		<div class="headers">
-		<h3 class="title">audio post</h3>
-		<h3>production</h3>
-		</div>
-</div>
-</div>
 
 <div class="row three">
-	<div class="layer">
+	<div class="layer layer-three">
 		<div class="headers">
-		<h3 class="title">meta data</h3>
-		<h3>solutions</h3>
+		<h3 class="title">Meta Data Solutions</h3>
+		<p class="row-description">Our team of trained in house curators provide end to end metadata entry for digital assets. We have experts that can learn and cater to our clients specific taxonomy and aggregate new metadata with preexisting metadata to help clients organize their content.</p>
 		</div>
 </div>
 </div>
 
 <div class="row four">
-<div class="layer">
+<div class="layer layer-four">
 		<div class="headers">
-		<h3 class="title">virtual reality</h3>
-		<h3>post production</h3>
+		<h3 class="title">Image/Video Solutions</h3>
+		<p class="row-description">Our editors are experts in all things digital media. Otomo Digital provides clients with easy solutions for all of their post production needs. We specialize in creating content ready to sell on the stock marketplace as well as VR-360 post production and IP/Object removal for both photo and video.</p>
 		</div>
 </div>
 </div>
